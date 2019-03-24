42e52a356b4ac3a7801f023a0928dadedcc3f50f
christian <christian.thibeault@umontreal.ca>
2019-03-24 18:00:26 -0400

    First commit: -Python script to generate table for index.html -Table in index.html contains links for individual cluster pages

---
+++ Web/Testing/Artificial_results/Results_table.js
@@ -0,0 +1,19 @@
+var full_Table = '<table id="Analysis_summary">'+
+'<th>'+
+'<td>Cluster Name</td>'+
+'<td>T</td>'+
+'<td>L</td>'+
+'<td>IsmyFavorite</td>'+
+'</th>'+
+'<tr>'+
+'<td>1</td><td><a href="Web/Testing/Artificial_results/Cluster_pages/Foo.html">Foo</a></td><td>1000</td><td>43</td><td>True</td>'+
+'</tr>'+
+'<tr>'+
+'<td>2</td><td><a href="Web/Testing/Artificial_results/Cluster_pages/bar.html">bar</a></td><td>4000</td><td>20</td><td>False</td>'+
+'</tr>'+
+'<tr>'+
+'<td>3</td><td><a href="Web/Testing/Artificial_results/Cluster_pages/lemur.html">lemur</a></td><td>30000</td><td>0.1</td><td>True</td>'+
+'</tr>'+
+'</table>'
+
+document.write(full_Table);
