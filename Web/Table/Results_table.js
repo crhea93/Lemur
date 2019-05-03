9394c68e4edae804c0992ed4915af028f31d3ce4
Carter Lee Rhea <carter.rhea@duke.edu>
2019-05-03 14:31:13 -0400

    Redid individual Cluster Pages

--- Web/Table/Results_table.js
+++ Web/Table/Results_table.js
@@ -2,20 +2,22 @@ var full_Table = '<table id="Analysis_summary">'+
 '<thead>'+
 '<tr class="table100-head">'+
 '<th>Name</th>'+
-'<th>ra</th>'+
-'<th>dec</th>'+
 '<th>redshift</th>'+
+'<th>RightAsc</th>'+
+'<th>Declination</th>'+
+'<th>R_cool_3</th>'+
+'<th>R_cool_7</th>'+
 '<th>Obsids</th>'+
 '</tr>'+
 '</thead>'+
 '<tr>'+
-'<td class="column0"><a href="../ClusterPages/Abell133.html" target="_blank">Abell133</a></td><td class="column0">None</td><td class="column0">None</td><td class="column0">0.0566</td><td class="column0">0</td>'+
+'<td class="column0"><a href="../ClusterPages/Abell85.html" target="_blank">Abell85</a></td><td class="column0">0.0558</td><td class="column0">00:41:50.232</td><td class="column0">-09:18:09.29</td><td class="column0">65.03</td><td class="column0">125.33</td><td class="column0">904</td>'+
 '</tr>'+
 '<tr>'+
-'<td class="column0"><a href="../ClusterPages/Abell85.html" target="_blank">Abell85</a></td><td class="column0">None</td><td class="column0">None</td><td class="column0">0.0558</td><td class="column0">904</td>'+
+'<td class="column0"><a href="../ClusterPages/Abell133.html" target="_blank">Abell133</a></td><td class="column0">0.0566</td><td class="column0">01:02:39.996</td><td class="column0">-21:52:47.08</td><td class="column0">59.18</td><td class="column0">0.0</td><td class="column0">2203,9897</td>'+
 '</tr>'+
 '<tr>'+
-'<td class="column0"><a href="../ClusterPages/RXCJ1115.html" target="_blank">RXCJ1115</a></td><td class="column0">None</td><td class="column0">None</td><td class="column0">0.35</td><td class="column0">3275,1</td>'+
+'<td class="column0"><a href="../ClusterPages/Ophiuchus.html" target="_blank">Ophiuchus</a></td><td class="column0">0.028</td><td class="column0">17:12:31.374</td><td class="column0">-23:19:06.17</td><td class="column0">42.22</td><td class="column0">118.19</td><td class="column0">3275</td>'+
 '</tr>'+
 '</table>'
 
