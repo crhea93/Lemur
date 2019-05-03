e3d29e3a539ce6b886ac1585179e22c61d23dadd
Carter Lee Rhea <carter.rhea@duke.edu>
2019-05-03 15:20:38 -0400

    Changed plot size issue
    Plots now scale with web page size :)

--- Web/Table/Results_table.js
+++ Web/Table/Results_table.js
@@ -14,7 +14,7 @@ var full_Table = '<table id="Analysis_summary">'+
 '<td class="column0"><a href="../ClusterPages/Abell85.html" target="_blank">Abell85</a></td><td class="column0">0.0558</td><td class="column0">00:41:50.232</td><td class="column0">-09:18:09.29</td><td class="column0">65.03</td><td class="column0">125.33</td><td class="column0">904</td>'+
 '</tr>'+
 '<tr>'+
-'<td class="column0"><a href="../ClusterPages/Abell133.html" target="_blank">Abell133</a></td><td class="column0">0.0566</td><td class="column0">01:02:39.996</td><td class="column0">-21:52:47.08</td><td class="column0">59.18</td><td class="column0">0.0</td><td class="column0">2203,9897</td>'+
+'<td class="column0"><a href="../ClusterPages/Abell133.html" target="_blank">Abell133</a></td><td class="column0">0.0566</td><td class="column0">01:02:41.957</td><td class="column0">-21:52:54.95</td><td class="column0">57.14</td><td class="column0">110.05</td><td class="column0">2203,9897</td>'+
 '</tr>'+
 '<tr>'+
 '<td class="column0"><a href="../ClusterPages/Ophiuchus.html" target="_blank">Ophiuchus</a></td><td class="column0">0.028</td><td class="column0">17:12:31.374</td><td class="column0">-23:19:06.17</td><td class="column0">42.22</td><td class="column0">118.19</td><td class="column0">3275</td>'+
