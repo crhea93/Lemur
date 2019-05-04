8d453e97014943678c6435049b00b00b13d29e9f
carterrhea <carterrhea93@gmail.com>
2019-05-04 11:41:15 -0400

    Primary Test Cases Ready
    Completely finished 3 primary test cases.
    Made the site mobile friendly.

--- Web/Table/Results_table.js
+++ Web/Table/Results_table.js
@@ -17,7 +17,7 @@ var full_Table = '<table id="Analysis_summary">'+
 '<td class="column0"><a href="../ClusterPages/Abell133.html" target="_blank">Abell133</a></td><td class="column0">0.0566</td><td class="column0">01:02:41.957</td><td class="column0">-21:52:54.95</td><td class="column0">57.14</td><td class="column0">110.05</td><td class="column0">2203,9897</td>'+
 '</tr>'+
 '<tr>'+
-'<td class="column0"><a href="../ClusterPages/Ophiuchus.html" target="_blank">Ophiuchus</a></td><td class="column0">0.028</td><td class="column0">17:12:31.374</td><td class="column0">-23:19:06.17</td><td class="column0">42.22</td><td class="column0">118.19</td><td class="column0">3275</td>'+
+'<td class="column0"><a href="../ClusterPages/Ophiuchus.html" target="_blank">Ophiuchus</a></td><td class="column0">0.028</td><td class="column0">17:12:27.624</td><td class="column0">-23:22:07.23</td><td class="column0">43.37</td><td class="column0">116.98</td><td class="column0">3275</td>'+
 '</tr>'+
 '</table>'
 
