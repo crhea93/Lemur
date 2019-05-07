b38d63f1f02bead42de71fc0940f1867a2a4a3f6
Carter Lee Rhea <carter.rhea@duke.edu>
2019-05-07 16:50:48 -0400

    Changes

--- Web/Filter_Table/value.php
+++ Web/Filter_Table/value.php
@@ -1,4 +1,3 @@
-
 <?php
 
 
@@ -16,25 +15,57 @@ if ($conn->connect_error) {
 }
 $search = $_POST['Value1'];
 $search2 = $_POST['Value2'];
-$sql = "SELECT $search,$search2 FROM Clusters";
+$sql = "SELECT $search,$search2 FROM Clusters ORDERED BY ClusterName";
 $result = $conn->query($sql);
 
 $arr1 = array();
 $arr2 = array();
+$arr1_err = array();
+$arr2_err = array();
+
+#check if we need to get the errors
+if ($search == 'csb_ct' || 'csb_pho') {
+    $sql = "SELECT $search+'_l',$search2+'_l' FROM csb ORDERED BY ClusterName";
+    $result_err1 = $conn->query($sql);
+};
+
 if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
         #echo "Redshift: " . $row["redshift"]."<br>";
         #$arr= array();
         #$arr['redshift'] = $row['redshift'];
         #$arr['id'] = $row['id'];
-        array_push($arr1,$row[$search]);
-        array_push($arr2,$row[$search2]);
+        array_push($arr1, $row[$search]);
+        array_push($arr2, $row[$search2]);
         #echo json_encode($arr);
         #echo json_encode($row['redshift']);
         //echo json_encode(array('redshift'=>$row['redshift'],'ID'=>$row['ID']));
     }
-    echo json_encode(array('Value1'=>$arr1,'Value2'=>$arr2));
+};
+
+/*if ($result_err1->num_rows > 0) {
+    // output data of each row
+    while($row = $result_err1->fetch_assoc()) {
+        #echo "Redshift: " . $row["redshift"]."<br>";
+        #$arr= array();
+        #$arr['redshift'] = $row['redshift'];
+        #$arr['id'] = $row['id'];
+        array_push($arr1_err,$row[$search+'_l']);
+        array_push($arr2_err,$row[$search2+"_l"]);
+        #echo json_encode($arr);
+        #echo json_encode($row['redshift']);
+        //echo json_encode(array('redshift'=>$row['redshift'],'ID'=>$row['ID']));
+    }
+}
+else {
+    $arr1_err = array_fill(0, $result->num_rows, 0);
+    $arr2_err = array_fill(0, $result->num_rows, 0);
+}*/
+
+
+if ($result->num_rows > 0) {
+    echo json_encode(array('Value1'=>$arr1,'Value2'=>$arr2));//,'err1'=>$arr1_err,'err2'=>$arr2_err));
 } else {
     echo "Couldn't Find Cluster";
 }
