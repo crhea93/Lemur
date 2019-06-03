88274949f1ac377d5fa97bc286a882d2a6fd4761
Carter Lee Rhea <carter.rhea@duke.edu>
2019-06-03 11:10:09 -0400

    Major Changes
    Cleaned table and restructured files

---
+++ Web/ClusterPages/js/A2029_obsid.js
@@ -0,0 +1,13 @@
+var Obsids = '<h2 style="color:white" class="FigureHeaders">10434</h2>'+
+'<div class="wrap-table100">'+
+' <div class="table100">'+
+'  <table>   <thead>'+
+'   <tr class="table100-head">'+
+'   <th>CCD IMAGE</th>'+
+'   <th>Background Flare</th>'+
+'   </tr>   </thead>  <tr>'+
+'   <td>     <img src=../Cluster_plots/A2029/10434_ccds.png alt="ccds" width="100%">    </td>'+
+'   <td>     <img src=../Cluster_plots/A2029/Lightcurve.png alt="LightCurve" width="100%">    </td>'+
+'   </tr>   </table>  </div> </div>'+
+''
+document.write(Obsids);
---
+++ Web/ClusterPages/js/MACSJ1447_obsid.js
@@ -0,0 +1,24 @@
+var Obsids = '<h2 style="color:white" class="FigureHeaders">17233</h2>'+
+'<div class="wrap-table100">'+
+' <div class="table100">'+
+'  <table>   <thead>'+
+'   <tr class="table100-head">'+
+'   <th>CCD IMAGE</th>'+
+'   <th>Background Flare</th>'+
+'   </tr>   </thead>  <tr>'+
+'   <td>     <img src=../Cluster_plots/MACSJ1447/17233_ccds.png alt="ccds" width="100%">    </td>'+
+'   <td>     <img src=../Cluster_plots/MACSJ1447/Lightcurve.png alt="LightCurve" width="100%">    </td>'+
+'   </tr>   </table>  </div> </div>'+
+'<h2 style="color:white" class="FigureHeaders">18825</h2>'+
+'<div class="wrap-table100">'+
+' <div class="table100">'+
+'  <table>   <thead>'+
+'   <tr class="table100-head">'+
+'   <th>CCD IMAGE</th>'+
+'   <th>Background Flare</th>'+
+'   </tr>   </thead>  <tr>'+
+'   <td>     <img src=../Cluster_plots/MACSJ1447/18825_ccds.png alt="ccds" width="100%">    </td>'+
+'   <td>     <img src=../Cluster_plots/MACSJ1447/Lightcurve.png alt="LightCurve" width="100%">    </td>'+
+'   </tr>   </table>  </div> </div>'+
+''
+document.write(Obsids);
