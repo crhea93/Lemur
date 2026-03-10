c247c80f7edb3ed3eec6b961a5f124a872f8b1af
Carter Rhea <carterrhea93@gmail.com>
2026-03-10 12:32:31 -0400

    Additional cleanup

--- Pipeline/web_export.py
+++ Pipeline/web_export.py
@@ -1,4 +1,21 @@
-from Misc.PlotToWeb import plots_to_web
+import os
+import shutil
+
+
+def plots_to_web(dir, obsids, name, web_dir):
+    if not os.path.exists(web_dir):
+        os.mkdir(web_dir)
+    for obsid in obsids:
+        os.chdir(dir + "/" + obsid + "/Background")
+        if os.path.exists(obsid + "_ccds.png"):
+            shutil.copyfile(obsid + "_ccds.png", web_dir + "/" + obsid + "_ccds.png")
+        if os.path.exists(obsid + "_Lightcurve.png"):
+            shutil.copyfile(
+                obsid + "_Lightcurve.png", web_dir + "/" + obsid + "_Lightcurve.png"
+            )
+    os.chdir(dir + "/" + name)
+    if os.path.exists("bkgsub_exp.png"):
+        shutil.copyfile("bkgsub_exp.png", web_dir + "/" + "bkgsub_exp.png")
 
 
 def export_web(inputs):
